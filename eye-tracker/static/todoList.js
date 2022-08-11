f2df586c08245afd927f82a4254b8dd874cb2aaa
Kyle Jung <kyle12jung@gmail.com>
2022-08-11 16:02:49 -0500

    todo ADD_TODO action implemented

--- eye-tracker/static/todoList.js
+++ eye-tracker/static/todoList.js
@@ -1,9 +1,44 @@
+// States
+// todo format { text: "", completed = True }
+let APP_STATE = {
+    totalItems: 0,
+    todo: [],
+}
+
+// reducer
 const reducer = (state = [], action) => {
     console.log("reducer", state, action);
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [...state, { text: action.payload, completed: false }]
+    }
 
 }
 const store = Redux.createStore(reducer);
 
+// Elements
+const todos = document.querySelector('.todos');
+const completed = document.querySelector('.completed');
+const userInput = document.querySelector('.user-input');
+const addButton = document.querySelector('.add');
 
-const content = document.querySelector('#list');
-content.innerHTML = "Deployed real-time computer vision stream into a web application"
+// dispatch is similar to setState (in react, onClick setState)
+addButton.addEventListener("click", () => {
+    store.dispatch({ type: "ADD_TODO", payload: userInput.value });
+})
+
+// subscribe (sort of like useEffect)
+store.subscribe(() => {
+    todos.innerHTML = "";
+    userInput.value = "";
+    store.getState().forEach((todo) => {
+        const span = document.createElement('span');
+        const checkBox = document.createElement("input");
+        checkBox.setAttribute("type", "checkbox");
+        const todoText = document.createElement('p');
+        todoText.innerText = todo.text;
+        span.appendChild(checkBox);
+        span.appendChild(todoText)
+        todos.appendChild(span)
+    })
+})
